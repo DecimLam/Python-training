cf595bed90a29fe5c74b445e7bb70e998803baab
DecimLam <thynhalam919@gmail.com>
2023-08-06 19:16:25 +0700

    init commit

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+#Open Settings: ctrl + ,
+
+#My first program in Python
+print("Hello World!")
+print("My name is Decim")
+
+#Find emoji: Win + .
+#Cannot print emoji
+print("❤️")
+print("Decim ❤️ Kimsun")
